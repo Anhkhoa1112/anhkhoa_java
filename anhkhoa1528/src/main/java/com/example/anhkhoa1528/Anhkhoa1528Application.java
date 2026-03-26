e50c2cbf584a05b57f9359c0bfea5f14ac58eaf9
Anhkhoa1112 <khoanguyen1.140490@gmail.com>
2026-03-26 14:24:40 +0700

    UPDATE

---
+++ anhkhoa1528/src/main/java/com/example/anhkhoa1528/Anhkhoa1528Application.java
@@ -0,0 +1,13 @@
+package com.example.anhkhoa1528;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Anhkhoa1528Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Anhkhoa1528Application.class, args);
+	}
+
+}
---
+++ anhkhoa1528/src/test/java/com/example/anhkhoa1528/Anhkhoa1528ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anhkhoa1528;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Anhkhoa1528ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
